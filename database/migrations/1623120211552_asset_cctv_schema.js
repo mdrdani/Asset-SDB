32a2f0fcb7534c183f470ae73af19f6c7a2935ab
mdrdani <sudokill3004@gmail.com>
2021-06-08 09:51:25 +0700

    Model & Migration Table CCTV

---
+++ database/migrations/1623120211552_asset_cctv_schema.js
@@ -0,0 +1,27 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class AssetCctvSchema extends Schema {
+  up () {
+    this.create('asset_cctvs', (table) => {
+      table.increments()
+      table.date('tanggal')
+      table.string('keterangan');
+      table.string('serialnumber');
+      table.string('bagianunit');
+      table.integer('quantity');
+      table.bigInteger('harga');
+      table.text('kendala').nullable();
+      table.string('gambar').nullable();
+      table.timestamps()
+    })
+  }
+
+  down () {
+    this.drop('asset_cctvs')
+  }
+}
+
+module.exports = AssetCctvSchema
